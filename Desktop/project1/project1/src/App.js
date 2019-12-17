41fe620ff60e2d0cc59b0eea6d1ec023315c9239
Josiane Santiago <josianesborrego@gmail.com>
2019-12-17 06:56:54 -0300

    iniciado projeto1

---
+++ Desktop/project1/project1/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './style.css';
+
+import Header from './Components/Header';
+
+
+
+
+const App = () => (
+    <div className="App">
+      <Header />
+    </div>
+  );
+
+
+export default App;
